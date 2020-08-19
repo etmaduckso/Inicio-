d2e4d06601d54ef6f701d45bc48f26d7b1deb42b
Sr.Lake <lake.mentoria@gmail.com>
2020-08-19 01:38:19 -0300

    .

--- NET/Program.cs
+++ NET/Program.cs
@@ -4,10 +4,16 @@ namespace NET
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 20;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                
+                Console.WriteLine($"Putaria do Agreste para o Hello World!{i}");
+            }
+            
         }
     }
 }
